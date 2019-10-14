e12e46a56c26bf9a700b13d6dacc3e6729bfe6ca
odonneb4 <odonneb4@tcd.ie>
2019-10-14 13:52:22 +0100

    Added more tests

--- src/lowestCommonAncestorTest.java
+++ src/lowestCommonAncestorTest.java
@@ -15,7 +15,6 @@ class lowestCommonAncestorTest {
 		tree.put(2);
 		tree.put(3);
 		assertEquals("The root will be returned, as it is the LCA",(Integer)1,tree.lowestCommonAncestor(1,2));
-		assertEquals("The root will be returned, as it is the LCA",(Integer)1,tree.lowestCommonAncestor(2,3));
 	}
 	@Test
 	public void testingInputs() {
@@ -33,7 +32,24 @@ class lowestCommonAncestorTest {
 		assertEquals("One input is a node not in the tree, thus it will return null",null,tree.lowestCommonAncestor("Christoper", "Philip"));
 		assertEquals("Two input is a node not in the tree, thus it will return null",null,tree.lowestCommonAncestor("Christoper", "Clare"));
 	}
-
+	@Test
+	public void testingBiggerTree() {
+		lowestCommonAncestor<Integer> tree = new lowestCommonAncestor<Integer>();
+		tree.put(10);	//				10
+		tree.put(8);	//			   /  \
+		tree.put(11);	//			  8	   11
+		tree.put(6);	//			 / \								
+		tree.put(3);	//			6	9
+		tree.put(4);	//		   / \
+		tree.put(7);	//		  3	  7
+		tree.put(9);	//		 / \
+		tree.put(2);	//		2	4
+		tree.put(5);	//	   /	 \
+		tree.put(1);	//	  1		  5
+		assertEquals("The tree is empty, thus it will return null",(Integer)6,tree.lowestCommonAncestor(5,6));
+		assertEquals("The tree has one node, thus it will return null",(Integer)10,tree.lowestCommonAncestor(8,11));
+		assertEquals("The root will be returned, as it is the LCA",(Integer)3,tree.lowestCommonAncestor(1,5));
+	}
 	
 	
 }
