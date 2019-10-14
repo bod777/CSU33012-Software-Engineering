7603360d9c3b0e95389c40c32ac4ed63b970e2ab
odonneb4 <odonneb4@tcd.ie>
2019-10-14 16:47:17 +0100

    Add tests for specific types, aka testingPutForInteger, testEmptyForString etc.

--- src/lowestCommonAncestorTest.java
+++ src/lowestCommonAncestorTest.java
@@ -3,6 +3,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+/* NEED TO ADD IN assertEquals comments */
+
+
 class lowestCommonAncestorTest {
 
 	@Test
@@ -17,7 +20,32 @@ class lowestCommonAncestorTest {
 		assertEquals("The root will be returned, as it is the LCA",(Integer)1,tree.lowestCommonAncestor(1,2));
 	}
 	@Test
-	public void testingNonExistentNodes() {
+	public void testingEmptyForString() {
+		lowestCommonAncestor<String> tree = new lowestCommonAncestor<String>();
+		
+		assertEquals("The tree is empty, thus it will return null",null,tree.lowestCommonAncestor("Cow","Dog"));
+		tree.put("Cow");
+		assertEquals("The tree has one node, thus it will return null",null,tree.lowestCommonAncestor("Cow","Dog"));
+		tree.put("Dog");
+		tree.put("Cat");
+		assertEquals("The root will be returned, as it is the LCA","Cow",tree.lowestCommonAncestor("Cat","Dog"));
+	}
+	@Test
+	public void testingNonExistentNodesForInteger() {
+		lowestCommonAncestor<Integer> tree = new lowestCommonAncestor<Integer>();
+		tree.put(10);
+		tree.put(6);
+		tree.put(11);
+		tree.put(13);
+		tree.put(9);
+		
+		assertEquals("One input is null, thus it will return null",null,tree.lowestCommonAncestor(null,6));
+		assertEquals("Two input is null, thus it will return null",null,tree.lowestCommonAncestor(null, null));
+		assertEquals("One input is a node not in the tree, thus it will return null",null,tree.lowestCommonAncestor(7,10));
+		assertEquals("Two input is a node not in the tree, thus it will return null",null,tree.lowestCommonAncestor(7,100));
+	}
+	@Test
+	public void testingNonExistentNodesForString() {
 		lowestCommonAncestor<String> tree = new lowestCommonAncestor<String>();
 		tree.put("Bríd");
 		tree.put("Marie");
@@ -33,7 +61,7 @@ class lowestCommonAncestorTest {
 		assertEquals("Two input is a node not in the tree, thus it will return null",null,tree.lowestCommonAncestor("Christoper", "Clare"));
 	}
 	@Test
-	public void testingTree() {
+	public void testingTreeForInteger() {
 		lowestCommonAncestor<Integer> tree = new lowestCommonAncestor<Integer>();
 		tree.put(10);	//				10
 		tree.put(8);	//			   /  \
@@ -52,39 +80,69 @@ class lowestCommonAncestorTest {
 	}
 	
 	@Test
-	public void testingOnesidedTree() {
+	public void testingTreeForString() {
+		lowestCommonAncestor<String> tree = new lowestCommonAncestor<String>();
+		tree.put("cow");	
+		tree.put("cat");	
+		tree.put("dog");	
+		tree.put("monkey");									
+		tree.put("ant");	
+		tree.put("pig");	
+		tree.put("fish");	
+
+		assertEquals("[]","cow",tree.lowestCommonAncestor("ant","monkey"));
+		assertEquals("[]","monkey",tree.lowestCommonAncestor("fish","pig"));
+		assertEquals("[]","dog",tree.lowestCommonAncestor("monkey","dog"));
+		tree.put("");
+		assertEquals("[]","cow",tree.lowestCommonAncestor("","dog"));
+	}
+	
+	@Test
+	public void testingOnesidedTreeForInteger() {
 		lowestCommonAncestor<Integer> tree = new lowestCommonAncestor<Integer>();
-		tree.put(10);	//				10
-		tree.put(9);	//			   /  
-		tree.put(8);	//			  9	   
-		tree.put(7);	//			 / 								
-		tree.put(6);	//			8	
-		tree.put(5);	//		   / 
-						//		  7	 
-						//		 / 
-						//		6	
-						//	   /	 
-						//	  5		
+		tree.put(10);	
+		tree.put(9);	  
+		tree.put(8);		   
+		tree.put(7);							
+		tree.put(6);		
+		tree.put(5);		
 		assertEquals("[]",(Integer)6,tree.lowestCommonAncestor(5,6));
 		assertEquals("[]",(Integer)9,tree.lowestCommonAncestor(7,9));
 		assertEquals("[]",(Integer)10,tree.lowestCommonAncestor(5,10));	
 	}
 	
 	@Test
-	public void testingPut() {
+	public void testingPutForInteger() {
 		lowestCommonAncestor<Integer> tree = new lowestCommonAncestor<Integer>();
 		tree.put(null);
 		assertEquals("[]","()",tree.printKeysInOrder());
-		tree.put(10);	//				10
+		tree.put(10);	
 		assertEquals("[]","(()10())",tree.printKeysInOrder());
-		tree.put(8);	//			   /  \
-		tree.put(11);	//			  8	   11
+		tree.put(8);	
+		tree.put(11);	
 		assertEquals("[]","((()8())10(()11()))",tree.printKeysInOrder());
-		tree.put(6);	//			 / \								
-		tree.put(9);	//			6	9
+		tree.put(6);							
+		tree.put(9);	
 		assertEquals("[]","(((()6())8(()9()))10(()11()))",tree.printKeysInOrder());
-		tree.put(7);	//			  \
-						//			   7
+		tree.put(7);	
 		assertEquals("[]","(((()6(()7()))8(()9()))10(()11()))",tree.printKeysInOrder());
+		tree.put(8);	
+		assertEquals("[]","(((()6(()7()))8(()9()))10(()11()))",tree.printKeysInOrder());
+	}
+	
+	@Test
+	public void testingPutForString() {
+		lowestCommonAncestor<String> tree = new lowestCommonAncestor<String>();
+		tree.put(null);
+		assertEquals("[]","()",tree.printKeysInOrder());
+		tree.put("Bríd");	
+		assertEquals("[]","(()Bríd())",tree.printKeysInOrder());
+		tree.put("Marie");	
+		tree.put("Philip");	
+		assertEquals("[]","(()Bríd(()Marie(()Philip())))",tree.printKeysInOrder());
+		tree.put("");	
+		assertEquals("[]","((()())Bríd(()Marie(()Philip())))",tree.printKeysInOrder());
+		tree.put("Marie");	
+		assertEquals("[]","((()())Bríd(()Marie(()Philip())))",tree.printKeysInOrder());
 	}
 }
